85b35acb8f9e2942b90e899961b4542d798030bd
Tony Hallett <tonyhallett74@gmail.com>
2025-02-28 16:47:03 +0000

    reflect master - use IProjectFilePropertyWriter

---
+++ SharedProject/Core/Utilities/IProjectFilePropertyWriter.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+
+namespace FineCodeCoverage.Core.Utilities
+{
+    public interface IProjectFilePropertyWriter
+    {
+        System.Threading.Tasks.Task<bool> RemovePropertyAsync(IVsHierarchy pHierProj, string propertyName);
+        System.Threading.Tasks.Task<bool> WritePropertyAsync(IVsHierarchy projectHierarchy, string propertyName, string value);
+    }
+}
