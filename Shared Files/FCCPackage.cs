31f90fbc89cccaaa8770be6f30c1e3e0a3bf7be5
Tony Hallett <tonyhallett74@gmail.com>
2025-05-18 12:17:06 +0100

    backup

--- Shared Files/FCCPackage.cs
+++ Shared Files/FCCPackage.cs
@@ -28,14 +28,12 @@ namespace FineCodeCoverage
     /// </summary>
     internal sealed partial class PackageIds
     {
-        public const int MyMenuGroup = 0x1020;
+        public const int FCCToolsMenuGroup = 0x1020;
         public const int ReportToolWindowToolbar = 0x1021;
         public const int ReadGroup = 0x1022;
         public const int FeedbackGroup = 0x1023;
         public const int ReportDetailsGroup = 0x1024;
         public const int ConfigurationGroup = 0x1025;
-        public const int SplitButtonMenuGroup = 0x1026;
-        public const int SplitButtonMenu = 0x1027;
         public const int RunGroup = 0x1028;
         public const int cmdidOpenReportWindowCommand = 0x0001;
         public const int cmdidToggleCoverageIndicatorsCommand = 0x0002;
