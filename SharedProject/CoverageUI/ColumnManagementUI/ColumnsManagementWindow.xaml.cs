288db6c243576288419491d2b6ad2cb6140870de
Tony Hallett <tonyhallett74@gmail.com>
2025-07-11 15:09:39 +0100

    backup

--- SharedProject/CoverageUI/ColumnManagementUI/ColumnsManagementWindow.xaml.cs
+++ SharedProject/CoverageUI/ColumnManagementUI/ColumnsManagementWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace FineCodeCoverage.Output
     /// <summary>
     /// report columns management window.
     /// </summary>
-    internal sealed partial class ColumnsManagementWindow2 : ThemedDialogWindow
+    internal sealed partial class ColumnsManagementWindow : ThemedDialogWindow
     {
         public ColumnsManagementWindow2(ReportColumnsManagementViewModel reportColumnsManagmentViewModel)
         {
---
+++ VsThemedDialogs/ThemedSectionDivider.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace VsThemedDialogs
+{
+    public partial class ThemedSectionDivider : UserControl
+    {
+        public ThemedSectionDivider() => InitializeComponent();
+    }
+}
